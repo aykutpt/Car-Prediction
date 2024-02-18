cf274d415f12f488ace132d80291903bff6ce84d
aykutpt <108530858+aykutpt@users.noreply.github.com>
2024-02-18 22:48:38 +0300

    car_prediction_streamlit.py

--- car_prediction_streamlit.py
+++ car_prediction_streamlit.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.linear_model import LinearRegression
 
 with open("car_prediction_model.pkl", "rb") as f:
     model = pickle.load(f)
